008151997710190fd9e5db111153fb67ea09d25f
Chris Gutierrez <cdotgutierrez@gmail.com>
2011-11-09 21:21:45 -0500

    completely rewrote the entire thing.

--- src/jquery.tagly.js
+++ src/jquery.tagly.js
@@ -28,63 +28,103 @@
         fieldName: 'tag',
         listClass: 'tag-list',
         labelClass: 'tag-label',
+        labelTextClass: 'tag-label-text',
         newClass: 'tagly-new',
-
     };
 
-    $.fn.tagly = function(opts) {
+    var Tagly = function($container, opts) {
+        
+        this.$container = $container.data('tagly', this);
+        this.$list = $container.find('ul');
+        this.opts = opts;
 
-        opts = $.extend(defaults, opts);
+        // get the list element containting the tags, if it doesn't exist...create one
+        if (!this.$list.length) {
+            this.$list = $('<ul class="' + opts.listClass + '">').appendTo($container[0]);
+        }
 
-        return this.each(function() {
-            var $this = $(this);
+        this.startNew();
 
-            function addTag() {
-                var tagtxt = this.val();
-
-                var tagMarkup = '<span class="' + opts.labelClass + '">' + tagtxt +
-                                    '<a class="' + opts.removeClass + '">' + opts.removeContent + '</a>\
-                                </span>\
-                                <input type="hidden" name="' + opts.fieldName + '[]" value="' + tagtxt + '"/>';
-
-                var $li = this.parent();
-
-                $li.append(tagMarkup);
-                this.remove();
-
-                $li.find('.' + opts.removeClass).click(function() {
-                    $li.remove();
-                });
-            }
-
-            function addNew() {
-                var tagMarkup = '<li>\
-                                    <input type="text" class="' + opts.newClass + '"/>\
-                                 </li>';
-
-                $newTag = this.append(tagMarkup).find('.' + opts.newClass);
-                $newTag.bind('keydown', onNewKeydown).focus();
-            }
-
-            function onNewKeydown(e) {
-                if (e.keyCode == 13) {
-                    e.preventDefault();
-                    addTag.apply($newTag);
-                    addNew.apply($list);
-                }
-            }
-
-            // get the list element containting the tags, if it doesn't exist...create one
-            $list = $this.find('ul');
-            if (!$list.length) {
-                $list = $this.append('<ul class="' + opts.listClass + '">').find('ul');
-            }
-
-            addNew.apply($list);
-
-            $this.click(function() {
-                $this.find('.' + opts.newClass).focus();
-            });
+        // focus on the input when the container is clicked
+        var self = this;
+        $container.click(function() {
+            $(this).find('.' + self.opts.newClass).focus();
         });
+    };
+
+    Tagly.prototype.addTag = function(val) {
+
+        var opts = this.opts,
+            $container = this.$container,
+            $lastli = this.$container.find('li:last');
+            
+        var markup = '<li>\
+                        <span class="' + opts.labelClass + '">\
+                        <span class="' + opts.labelTextClass + '">'+ val + '</span>\
+                        <a class="' + opts.removeClass + '">' + opts.removeContent + '</a>\
+                    </span>\
+                    <input type="hidden" name="' + opts.fieldName + '[]" value="' + val + '"/>\
+                    </li>';
+
+        $lastli.before(markup);
+        $lastli.find('input').val("");
+        
+        $lastli.prev().find('.' + opts.removeClass).bind('click.tagly', function(e) {
+            $(this).parents('li').remove();
+        });
+    };
+    
+    
+    Tagly.prototype.startNew = function() {
+        
+        var opts = this.opts,
+            $list = this.$list;
+        
+        var markup = '<li>\
+                        <input type="text" class="' + opts.newClass + '"/>\
+                      </li>';
+
+        var $input = $list.append(markup).find('.' + opts.newClass),
+            self = this;
+            
+        $input.bind('keydown.tagly', function(e) {
+            if (e.keyCode === 13 || e.keyCode === 9) {
+                e.preventDefault();
+                self.onNewKeydown();
+            }
+        }).focus();
+    };
+     
+    Tagly.prototype.onNewKeydown = function() {
+        var opts = this.opts,
+            $input = this.$list.find('.' + opts.newClass);
+
+        if ($.trim($input.val()).length) {
+            this.addTag($input.val());
+        }
+    };
+
+    Tagly.prototype.populate = function(tags) {
+        var self = this;
+        $.each(tags, function(i, tag) {
+            self.addTag(tag);
+        });
+    };
+
+    $.fn.tagly = function() {
+        var args = arguments,
+            cmd = (args.length) ? args[0] : {},
+            cmdtype = typeof cmd;
+        
+        if (cmdtype === "object" || !cmd) {
+            return this.each(function(i, elem) {
+                (new Tagly($(elem), $.extend(defaults, cmd || {})));
+            });
+        } else if(cmdtype === "string" && cmd in Tagly.prototype) {
+            return this.each(function(i, elem) {
+                var tagly = $(elem).data('tagly');
+                tagly[cmd].apply(tagly, Array.prototype.slice.call(args, 1));
+            });
+        }
     };
 })(jQuery);
